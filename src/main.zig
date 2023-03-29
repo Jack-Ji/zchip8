0a2e86e5e66a2a8092f693e1ee4af26c9bb9da91
jackji <jichengde@gmail.com>
2023-03-29 15:04:14 +0800

    basic init

--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,37 @@ const sdl = jok.sdl;
 const j2d = jok.j2d;
 const j3d = jok.j3d;
 
+const display_width = 64;
+const display_height = 32;
+
+/// CHIP-8 instance
+const Chip8 = struct {
+    /// CHIP-8 has direct access to up to 4 kilobytes of RAM
+    mem: []u8,
+
+    /// A program counter, often called just “PC”, which points at the current
+    /// instruction in memory The first CHIP-8 interpreter (on the COSMAC VIP
+    /// computer) was also located in RAM, from address 000 to 1FF. It would
+    /// expect a CHIP-8 program to be loaded into memory after it, starting at
+    /// address 200 (512 in decimal).
+    pc: usize = 0x200,
+
+    fn init(allocator: std.mem.Allocator) !Chip8 {
+        return .{
+            .mem = try allocator.alloc(u8, 4096),
+        };
+    }
+
+    fn deinit(self: *Chip8, allocator: std.mem.Allocator) void {
+        allocator.free(self.mem);
+        self.* = undefined;
+    }
+};
+
+var chip8: Chip8 = undefined;
+
 pub fn init(ctx: jok.Context) !void {
-    _ = ctx;
+    chip8 = try Chip8.init(ctx.allocator());
 }
 
 pub fn event(ctx: jok.Context, e: sdl.Event) !void {
@@ -34,5 +63,5 @@ pub fn draw(ctx: jok.Context) !void {
 }
 
 pub fn quit(ctx: jok.Context) void {
-    _ = ctx;
+    chip8.deinit(ctx.allocator());
 }
